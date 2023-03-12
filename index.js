0fc6d38ac65cef5df01c2d5e91e95982b5c7aeb4
Trenton Jones <mr.jones1213@yahoo.com>
2023-03-11 20:31:24 -0500

    submission for lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Trent',
+    streetAddress: '6811 Quander Rd',
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return{
+        ...employee,
+        [key]: value,
+}
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value,) {
+    employee[key] = value;
+    return employee;
+};
+function deleteFromEmployeeByKey(employee, key,) {
+    const newEmployee = { ...employee};
+    delete newEmployee[key];
+    return newEmployee;
+};
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    delete employee[key]
+    return employee
+};
